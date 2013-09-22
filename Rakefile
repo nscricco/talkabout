b058bf15f1f4afae9623bc73c644f5314cb513bc
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-21 20:53:19 -0700

    fix seed functionality

--- Rakefile
+++ Rakefile
@@ -3,5 +3,10 @@ require 'sinatra/activerecord/rake'
 
 desc 'drop the database'
 task 'db:drop' do
-	rm_f 'db/talkabout.db'
+	rm_f './mydb.sqlite3'
+end
+
+desc 'seed the database'
+task 'db:seed' do
+	ruby File.dirname(__FILE__) + '/db/seed.rb'
 end
--- db/seed.rb
+++ db/seed.rb
@@ -1,35 +1,41 @@
 require 'faker'
+require_relative '../app/config.rb'
 
-module Seed
-	def self.create_user
-		User.create(
-			firstname: Faker::Name.first_name
-			lastname: Faker::Name.last_name
-			email: Faker::Internet.email
+class Seeder
+	def create_user
+		user = User.create(
+			firstname: Faker::Name.first_name,
+			lastname: Faker::Name.last_name,
+			email: Faker::Internet.email,
 			password: Faker::Internet.password)
+		return user
 	end
 
-	def self.create_topic user
-		Topic.create(
-			title: Faker::Company.catch_phrase
-			body: Faker::Lorem.paragraphs(paragraph_count = (rand(3)+1), supplemental = false).join('</br>')
+	def create_topic user
+		topic = Topic.create(
+			title: Faker::Company.catch_phrase,
+			body: Faker::Lorem.paragraphs(paragraph_count = (rand(3)+1), supplemental = false).join('</br>'),
 			user: user)
+		return topic
 	end
 
-	def self.create_comment user, topic
-		Comment.create(
-			body: Faker::Lorem.paragraphs(paragraph_count = (rand(2)+1), supplemental = false).join('</br>')
-			topic: topic
+	def create_comment user, topic
+		comment = Comment.create(
+			body: Faker::Lorem.paragraphs(paragraph_count = (rand(2)+1), supplemental = false).join('</br>'),
+			topic: topic,
 			user: user)
+		return comment
 	end
 end
 
+mySeeder = Seeder.new
+
 10.times do
-	user = Seed.create_user
+	user = mySeeder.create_user
 	3.times do
-		topic = Topic.create_topic(user)
+		topic = mySeeder.create_topic(user)
 		5.times do
-			comment = Comment.create(user, topic)
+			comment = mySeeder.create_comment(user, topic)
 		end
 	end
 end
