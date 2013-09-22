9423bf6b122217a7efc2cffb6709c898e8ed2909
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-21 19:26:20 -0700

    update topic to meet model specs

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,4 +4,6 @@ require_relative '../config'
 class Topic < ActiveRecord::Base
 	belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 	has_many :comments
+
+	validates_presence_of :title, :body, :user
 end
