2e3183e934c0a812621104d046d2a324c192374a
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-21 18:27:58 -0700

    add user spec

---
+++ spec/model_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'User' do
+	include FeatureSpec
+
+	it 'should be invalid without an email and password' do
+		user = User.create
+		expect(user).not_to be_valid
+		User.destroy_all
+	end
+	it 'should be valid with an email and password' do
+		user = User.create(email: 'test@gmail.com', password: 'pass')
+		expect(user).to be_valid
+		User.destroy_all
+	end
+	it 'should return a user\'s name as a full string' do
+		user = User.create(firstname: 'first', lastname: 'last', email: 'test@gmail.com', password: 'pass')
+		expect(user.name).to include 'first last'
+	end
+end
