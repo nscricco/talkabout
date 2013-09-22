c4f691e504b188b98a07a7a566b11798a0848c99
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-21 18:58:07 -0700

    add user password spec

--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -3,23 +3,27 @@ require 'spec_helper'
 describe 'User' do
 	include Spec
 
-	it 'should be invalid without an email and password' do
-		user = User.create
+	it 'should be invalid without an email' do
+		user = User.create(password: 'pass')
 		expect(user).not_to be_valid
-		User.destroy_all
+	end
+	it 'should be invalid without a password' do
+		user = User.create(email: 'test@gmail.com')
+		expect(user).not_to be_valid
 	end
 	it 'should be valid with an email and password' do
 		user = User.create(email: 'test@gmail.com', password: 'pass')
 		expect(user).to be_valid
-		User.destroy_all
 	end
 	it 'should return a user\'s name as a full string' do
 		user = User.create(firstname: 'first', lastname: 'last', email: 'test@gmail.com', password: 'pass')
 		expect(user.name).to include 'first last'
-		User.destroy_all
 	end
+	User.destroy_all
 end
 
 describe 'Topic' do
 	include Spec
+
+	it 'should be invalid without a title'
 end
