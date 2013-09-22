d0dc443a294445fc907d0428d3a265e0518d64b9
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-21 23:08:17 -0700

    add topic page spec: display all comments

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -20,28 +20,29 @@ describe 'Homepage' do
 			expect(last_response.body).to include topic.user.name
 		end
 	end
-
-	User.destroy_all
-	Topic.destroy_all
-	Comment.destroy_all
 end
 
 describe 'Topic Page' do
 	include Spec
 
 	let!(:user) {User.create(email: 'test@gmail.com', password: 'pass')}
-	let!(:topic) {Topic.create(title: 'title', body: 'body', user: user)}
+	let!(:topic) {Topic.create(title: 'title', body: 'topic_body', user: user)}
+	let!(:comment) {Comment.create(body: 'comment_body', topic: topic, user: user)}
 
 	it 'should load' do
 		get "topic/#{topic.title}"
 		last_response.should be_ok
 	end
-	it 'should display the body' do
+	it 'should display the topic body' do
 		get "topic/#{topic.title}"
 		expect(last_response.body).to include topic.body
 	end
-
-	User.destroy_all
-	Topic.destroy_all
-	Comment.destroy_all
+	it 'should display the author' do
+		get "topic/#{topic.title}"
+		expect(last_response.body).to include topic.user.name
+	end
+	it 'should display all comments' do
+		get "topic/#{topic.title}"
+		expect(last_response.body).to include comment.body
+	end
 end
