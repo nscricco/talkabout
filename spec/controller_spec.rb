311474035394630fb680d179fbf553f733443213
Nicholas Scricco <nicholasscricco@Nicholass-MacBook-Air.local>
2013-09-22 11:31:58 -0700

    refactor controller_spec

--- spec/controller_spec.rb
+++ spec/controller_spec.rb
@@ -6,18 +6,22 @@ describe 'Homepage' do
 	let!(:user) {User.create(email: 'test@gmail.com', password: 'pass')}
 	let!(:topic) {Topic.create(title: 'title', body: 'body', user: user)}
 
-	it 'should load' do
+	before :each do
 		get '/'
+	end
+
+	it 'should load' do
 		last_response.should be_ok
 	end
 	it 'should show list of discussions' do
-		get '/'
 		expect(last_response.body).to include topic.title
 	end
 	describe 'discussions' do
 		it 'should display the author' do
-			get '/'
 			expect(last_response.body).to include topic.user.name
+		end
+		it 'should link to the discussion page' do
+			last_response.should link_to(href: "topic/#{topic.title}")
 		end
 	end
 end
@@ -30,24 +34,23 @@ describe 'Topic Page' do
 	let!(:comment_user) {User.create(firstname: 'John', email: 'JohnDoe@gmail.com', password: 'pass')}
 	let!(:comment) {Comment.create(body: 'comment_body', topic: topic, user: comment_user)}
 
-	it 'should load' do
+	before :each do
 		get "topic/#{topic.title}"
+	end
+
+	it 'should load' do
 		last_response.should be_ok
 	end
 	it 'should display the topic body' do
-		get "topic/#{topic.title}"
 		expect(last_response.body).to include topic.body
 	end
 	it 'should display the author' do
-		get "topic/#{topic.title}"
 		expect(last_response.body).to include topic.user.name
 	end
 	it 'should display all comments' do
-		get "topic/#{topic.title}"
 		expect(last_response.body).to include comment.body
 	end
 	it 'should display the comment author' do
-		get "topic/#{topic.title}"
 		expect(last_response.body).to include comment.user.name
 	end
 end
@@ -58,20 +61,20 @@ describe 'User Page' do
 	let!(:user) {User.create(firstname: 'John', lastname: 'Doe', email: 'test@gmail.com', password: 'pass')}
 	let!(:topic) {Topic.create(title: 'title', body: 'topic_body', user: user)}
 
-	it 'should load' do
+	before :each do
 		get "user/#{user.id}"
+	end
+
+	it 'should load' do
 		last_response.should be_ok
 	end
 	it 'should display the user\'s name' do
-		get "user/#{user.id}"
 		expect(last_response.body).to include user.name
 	end
 	it 'should display the user\'s email' do
-		get "user/#{user.id}"
 		expect(last_response.body).to include user.email
 	end
 	it 'should display titles of topics authored by the user' do
-		get "user/#{user.id}"
 		expect(last_response.body).to include topic.title
 	end
 end
